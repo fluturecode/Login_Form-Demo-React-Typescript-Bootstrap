20f30cd6e66558a04caa1c4487ed6048ca1db470
Elizabeth Eidelson <fluturecode@gmail.com>
2021-03-03 23:14:26 -0500

    added LoginPage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { LoginPage } from "./pages/home/LoginPage"
 import { HomePage } from "./pages/HomePage"
+
 function App() {
 	return (
 		<>
 			<HomePage />
-			{/* <LoginPage/>
-		<SignUpPage/> */}
+			<LoginPage />
+			{/* <SignUpPage/> */}
 		</>
 	)
 }
